270d137200306e6a009f80a9f58280c648350d88
oncleben31 <oncleben31@users.noreply.github.com>
2016-10-17 23:18:14 +0200

    Add ping search services to Rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -57,6 +57,52 @@ namespace :build do
   end
 end
 
+####################
+# Notification tasks
+####################
+
+# Usage: rake notify
+task :notify => ["notify:pingomatic", "notify:google", "notify:bing"]
+desc "Notify various services that the site has been updated"
+namespace :notify do
+
+  desc "Notify Ping-O-Matic"
+  task :pingomatic do
+    begin
+      require 'xmlrpc/client'
+      puts "* Notifying Ping-O-Matic that the site has updated"
+      XMLRPC::Client.new('rpc.pingomatic.com', '/').call('weblogUpdates.extendedPing', 'oncleben31.cc' , 'http://oncleben31.cc', 'http://oncleben31.cc/feed.xml')
+    rescue LoadError
+      puts "! Could not ping ping-o-matic, because XMLRPC::Client could not be found."
+    end
+  end
+
+  desc "Notify Google of updated sitemap"
+  task :google do
+    begin
+      require 'net/http'
+      require 'uri'
+      puts "* Notifying Google that the site has updated"
+      Net::HTTP.get('www.google.com', '/webmasters/tools/ping?sitemap=' + URI.escape('http://oncleben31.cc/sitemap.xml'))
+    rescue LoadError
+      puts "! Could not ping Google about our sitemap, because Net::HTTP or URI could not be found."
+    end
+  end
+
+  desc "Notify Bing of updated sitemap"
+  task :bing do
+    begin
+      require 'net/http'
+      require 'uri'
+      puts '* Notifying Bing that the site has updated'
+      Net::HTTP.get('www.bing.com', '/webmaster/ping.aspx?siteMap=' + URI.escape('http://onlceben31.cc/sitemap.xml'))
+    rescue LoadError
+      puts "! Could not ping Bing about our sitemap, because Net::HTTP or URI could not be found."
+    end
+  end
+end
+
+
 ##############
 # CI tasks
 ##############
